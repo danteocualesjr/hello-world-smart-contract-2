e5cd96703223e0a9771b8e3898fd8969aff27e68
Dante O. Cuales <danteocualesjr@gmail.com>
2022-03-13 09:55:48 +0800

    Saved hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,4 +8,12 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.7.3",
-};
+  defaultNetwork: "ropsten",
+  networks: {
+     hardhat: {},
+     ropsten: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     }
+  },
+}
